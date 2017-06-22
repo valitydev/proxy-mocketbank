65295dbd7b4914aae9149201ecb458ad58dd0bcf
Anatoly Cherkasov <shnurok14@ya.ru>
2017-06-22 17:34:44 +0300

    PROX-81: remove logs (#8)
    * PROX-81: remove logs

--- src/main/java/com/rbkmoney/proxy/mocketbank/utils/cds/CdsApi.java
+++ src/main/java/com/rbkmoney/proxy/mocketbank/utils/cds/CdsApi.java
@@ -56,7 +56,7 @@ public class CdsApi {
     public CardData getCardData(String token) throws TException {
         LOGGER.info("Storage getCardData: token {}", token);
         CardData cardData = storageSrv.getCardData(token);
-        LOGGER.info("Storage getCardData: response {}", cardData.toString());
+        LOGGER.info("Storage getCardData: response token {}", token);
         return cardData;
     }
 
@@ -71,7 +71,7 @@ public class CdsApi {
     public CardData getSessionCardData(String token, String session) throws TException {
         LOGGER.info("Storage getSessionCardData: token {}, session{} ", token, session);
         CardData cardData = storageSrv.getSessionCardData(token, session);
-        LOGGER.info("Storage getSessionCardData: response {}", cardData.toString());
+        LOGGER.info("Storage getSessionCardData: response token {}, session{}", token, session);
         return cardData;
     }
 
@@ -83,9 +83,9 @@ public class CdsApi {
      * @throws TException
      */
     public PutCardDataResult putCardData(CardData cardData) throws TException {
-        LOGGER.info("Storage putCardData: cardData {} ", cardData);
+        LOGGER.info("Storage putCardData: cardData");
         PutCardDataResult result = storageSrv.putCardData(cardData);
-        LOGGER.info("Storage putCardData: response {}", result.toString());
+        LOGGER.info("Storage putCardData: response");
         return result;
     }
 
