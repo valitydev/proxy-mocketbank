3e541e8c6ed0462863164bcad69250031e76f56e
ggmaleva <ggmaleva@yandex.ru>
2022-03-18 16:44:50 +0300

    add account balance service mock

---
+++ src/main/java/com/rbkmoney/proxy/mocketbank/handler/balance/AccountBalanceHandler.java
@@ -0,0 +1,41 @@
+package com.rbkmoney.proxy.mocketbank.handler.balance;
+
+import dev.vality.account_balance.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+@Component
+public class AccountBalanceHandler implements AccountServiceSrv.Iface {
+
+    public static final String WRONG_ARGUMENT_EXCEPTION_MESSAGE = "Empty options in account balance request";
+
+    @Override
+    public BalanceResponse getBalance(BalanceRequest balanceRequest) {
+        if (CollectionUtils.isEmpty(balanceRequest.getOptions())) {
+            throw new IllegalArgumentException(WRONG_ARGUMENT_EXCEPTION_MESSAGE);
+        }
+        String requestTime = balanceRequest.isSetRequestTime()
+                ? balanceRequest.getRequestTime()
+                : ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT);
+        log.info("Receive balance request dated {} with options: {}", requestTime, balanceRequest.getOptions());
+        Balance balance = new Balance();
+        balance.setCurrencyCode("EUR");
+        long amount = ThreadLocalRandom.current().nextLong(1000);
+        balance.setAmount(amount);
+        AccountReference accountReference = new AccountReference();
+        long accountId = ThreadLocalRandom.current().nextLong(10000);
+        accountReference.setId(accountId);
+        BalanceResponse response = new BalanceResponse()
+                .setAccountReference(accountReference)
+                .setResponseTime(ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT))
+                .setBalance(balance);
+        log.info("Response for account {} with balance {} {}", accountId, amount, balance.getCurrencyCode());
+        return response;
+    }
+}
---
+++ src/test/java/com/rbkmoney/proxy/mocketbank/handler/balance/AccountBalanceHandlerTest.java
@@ -0,0 +1,39 @@
+package com.rbkmoney.proxy.mocketbank.handler.balance;
+
+import dev.vality.account_balance.BalanceRequest;
+import dev.vality.account_balance.BalanceResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class AccountBalanceHandlerTest {
+
+    private final AccountBalanceHandler handler = new AccountBalanceHandler();
+
+    @Test
+    void getBalanceWithError() {
+        BalanceRequest request = new BalanceRequest();
+        request.setOptions(Collections.emptyMap());
+
+        var exception = assertThrows(IllegalArgumentException.class, () -> handler.getBalance(request));
+
+        assertEquals("Empty options in account balance request", exception.getMessage());
+    }
+
+    @Test
+    void getBalanceSuccess() {
+        BalanceRequest request = new BalanceRequest();
+        request.setOptions(Map.of(UUID.randomUUID().toString(), UUID.randomUUID().toString()));
+
+        BalanceResponse response = handler.getBalance(request);
+
+        assertNotNull(response);
+    }
+}
