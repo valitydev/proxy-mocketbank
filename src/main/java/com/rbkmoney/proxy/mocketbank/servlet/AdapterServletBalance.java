af90a29a92f1f8445a3c37751354dac778ebb68b
Gregory <32060161+ggmaleva@users.noreply.github.com>
2022-03-21 12:54:46 +0300

    add account balance service mock (#19)
    * add account balance service mock
    
    * refactoring + fix linter
    
    * fix linter
    
    * fix after review
    
    Co-authored-by: ggmaleva <ggmaleva@yandex.ru>

---
+++ src/main/java/com/rbkmoney/proxy/mocketbank/servlet/AdapterServletBalance.java
@@ -0,0 +1,30 @@
+package com.rbkmoney.proxy.mocketbank.servlet;
+
+import com.rbkmoney.proxy.mocketbank.handler.balance.AccountBalanceHandler;
+import com.rbkmoney.woody.thrift.impl.http.THServiceBuilder;
+import dev.vality.account_balance.AccountServiceSrv;
+import lombok.RequiredArgsConstructor;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@WebServlet("/proxy/mocketbank/balance")
+public class AdapterServletBalance extends GenericServlet {
+
+    private final AccountBalanceHandler handler;
+    private Servlet servlet;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        servlet = new THServiceBuilder().build(AccountServiceSrv.Iface.class, handler);
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        servlet.service(request, response);
+    }
+
+}
