078de5cc310bb9bb1ff88b37c16635591f98ad61
vitaxa <vitaxa93gamebox@gmail.com>
2022-04-19 17:27:19 +0300

    TD-267: refactor bankcard model access (#26)

--- src/main/java/com/rbkmoney/proxy/mocketbank/utils/qps/QpsExtractors.java
+++ src/main/java/com/rbkmoney/proxy/mocketbank/utils/qps/QpsExtractors.java
@@ -1,6 +1,6 @@
 package com.rbkmoney.proxy.mocketbank.utils.qps;
 
-import com.rbkmoney.damsel.domain.LegacyTerminalPaymentProvider;
+import com.rbkmoney.damsel.domain.PaymentServiceRef;
 import com.rbkmoney.damsel.domain.PaymentTool;
 import com.rbkmoney.damsel.proxy_provider.PaymentContext;
 import com.rbkmoney.damsel.proxy_provider.PaymentResource;
@@ -8,20 +8,24 @@ import com.rbkmoney.java.damsel.utils.extractors.ProxyProviderPackageExtractors;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Optional;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class QpsExtractors {
 
-    public static LegacyTerminalPaymentProvider extractTerminalPaymentProvider(PaymentContext context) {
+    public static String extractTerminalPaymentProvider(PaymentContext context) {
         PaymentResource paymentResource = ProxyProviderPackageExtractors.extractPaymentResource(context);
         if (paymentResource.isSetDisposablePaymentResource()) {
             PaymentTool paymentTool = paymentResource.getDisposablePaymentResource().getPaymentTool();
             if (paymentTool.isSetPaymentTerminal()) {
-                return paymentTool.getPaymentTerminal().getTerminalTypeDeprecated();
+                return Optional.ofNullable(paymentTool.getPaymentTerminal().getPaymentService())
+                        .map(PaymentServiceRef::getId).orElse(null);
             }
         } else if (paymentResource.isSetRecurrentPaymentResource()) {
             PaymentTool paymentTool = paymentResource.getRecurrentPaymentResource().getPaymentTool();
             if (paymentTool.isSetPaymentTerminal()) {
-                return paymentTool.getPaymentTerminal().getTerminalTypeDeprecated();
+                return Optional.ofNullable(paymentTool.getPaymentTerminal().getPaymentService())
+                        .map(PaymentServiceRef::getId).orElse(null);
             }
         }
         return null;
